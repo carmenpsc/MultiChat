862d0dfcfd9c671a5410c17e292889c7ab9293b7
carmenpsc <carrpsc94@outlook.com>
2017-03-26 22:20:50 +0200

    Cambiando url video

--- app_server/config.js
+++ app_server/config.js
@@ -6,7 +6,7 @@ var config = {};
 //Heroku assigns the value through the PORT environment variable. You cannot choose
 config.port = process.env.HTTP_PORT || process.env.PORT || 3000;
 
-config.videoUrl = 'http://156.35.98.18:32400/video/:/transcode/universal/start?hasMDE=1&path=%2Flibrary%2Fmetadata%2F1&mediaIndex=0&partIndex=0&protocol=http&fastSeek=1&directPlay=0&directStream=1&subtitleSize=100&audioBoost=100&location=lan&session=sxorjtw7t6bvwoxyjcekpo73&offset=0&subtitles=burn&copyts=1&Accept-Language=es&X-Plex-Session-Identifier=p9rzmazaw8p7csjrwzrqtoqg&X-Plex-Chunked=1&X-Plex-Product=Plex%20Web&X-Plex-Version=3.1.1&X-Plex-Client-Identifier=eiw5lt8hontf61ykmhd9cc1s&X-Plex-Platform=Chrome&X-Plex-Platform-Version=57.0&X-Plex-Device=Windows&X-Plex-Device-Name=Plex%20Web%20%28Chrome%29&X-Plex-Device-Screen-Resolution=1920x950%2C1920x1080&X-Plex-Token=HqpHZ537zej1ZniLqCRu';
+config.videoUrl = 'https://156-35-98-18.d6b6c198ddfc47509c7c7ab5c6b8a0ea.plex.direct:32400/library/parts/2/1490382404/file.webm?X-Plex-Session-Identifier=wly6kmb8nocpacgruqvy4u06&X-Plex-Product=Plex%20Web&X-Plex-Version=3.1.1&X-Plex-Client-Identifier=78b2ee41-e775-4899-ae1b-d3b8f27d378f&X-Plex-Platform=Chrome&X-Plex-Platform-Version=57.0&X-Plex-Device=Windows&X-Plex-Device-Name=Plex%20Web%20%28Chrome%29&X-Plex-Device-Screen-Resolution=1920x950%2C1920x1080&X-Plex-Token=Xd9VZQ5rfYqizdys7Azy&Accept-Language=es';
 config.audioUrl = 'http://156.35.98.18:8001/;'; //shoutcast (in .pls file)
 config.presentationUrl = 'reveal-js/index.html';
 
